3832b5d3638fb26e2d4277f2ec9c79388b89e347
leculette <ivandietert@gmail.com>
2023-01-21 21:49:52 -0300

    cert

---
+++ cert/payload_cert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+openssl req -x509 -sha256 -nodes -newkey rsa:4096 -keyout ./broscience.key -out ./broscience.crt -days 1 <<<"AU
+    Victoria
+    Melbourne
+    Broscience
+    Broscience
+    $1
+    Broscience
+    " 2>/dev/null
+
+    
+
